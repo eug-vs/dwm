4003fe5ead6d2400c6f45fc6b0bd95030644ebf0
eug-vs <eug-vs@keemail.me>
2021-03-27 21:40:35 +0300

    feat: apply swallow patch

--- config.def.h
+++ config.def.h
@@ -37,9 +37,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
 	// TODO: figure out firefox { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Alacritty",       NULL,       NULL,       0,            0,           1,         0,        -1 },
 };
 
 /* layout(s) */
